555ff06e92057eac3fb215f4a3bf4b1ef5cd4d2a
wuqinfa <369724377@qq.com>
2022-08-05 16:44:24 +0800

    chore: 加入引用

---
+++ packages/components/index.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: wuqinfa
+ * @Date: 2022-08-05 16:28:04
+ * @LastEditors: wuqinfa
+ * @Description: 
+ */
+export default () => {
+  console.log('packages/components');
+}
---
+++ packages/models/index.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: wuqinfa
+ * @Date: 2022-08-05 16:27:58
+ * @LastEditors: wuqinfa
+ * @Description: 
+ */
+export default () => {
+  console.log('packages/models');
+}
